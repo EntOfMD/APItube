c5e5ab60e11d2a14824e7f2699fa5cb504dcf36d
Ashell Fernando <ashellz2010@gmail.com>
2019-01-13 13:46:49 -0500

    added state, value, and onChange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
     render() {
         return (
             <div className="search-bar ui segment">
                 <form className="ui form">
                     <div className="field">
-                        <label className="title"></label>
-                        <input type="text" placeholder="type to search..." />
+                        <label className="title">
+                            justTheBud - a YouTube explorer app
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="type to search..."
+                            value={this.state.term}
+                            onChange={this.onInputChange}
+                        />
                     </div>
                 </form>
             </div>
