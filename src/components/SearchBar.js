410ad6b0085c14f40559be734b22a4b31ddfe985
Ashell Fernando <ashellz2010@gmail.com>
2019-01-13 13:51:23 -0500

    added onInputChange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,14 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = { term: '' };
 
+    onInputChange = event => {
+        this.setState({ term: event.target.value });
+    };
+
     render() {
         return (
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label className="title">
                             justTheBud - a YouTube explorer app
@@ -16,6 +20,7 @@ class SearchBar extends React.Component {
                             placeholder="type to search..."
                             value={this.state.term}
                             onChange={this.onInputChange}
+                            //onChange={e=> this.setState({term:e.target.value})}
                         />
                     </div>
                 </form>
