87fe93830505b1b0ada8e0a0663f620cd8ee2b75
Ashell Fernando <ashellz2010@gmail.com>
2019-01-13 13:37:51 -0500

    added semantic to searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,18 @@
 import React from 'react';
+
 class SearchBar extends React.Component {
-	render() {
-		return <div>Searchbar will go here</div>;
-	}
+    render() {
+        return (
+            <div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label className="title"></label>
+                        <input type="text" placeholder="type to search..." />
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default SearchBar;
