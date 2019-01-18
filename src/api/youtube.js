01a9e5a822cc177a2c1d4d1acac2a4636fbb59cf
Ashell Fernando <ashellz2010@gmail.com>
2019-01-18 11:59:36 -0500

    imp key and laying down foundation for search page

---
+++ src/api/youtube.js
@@ -0,0 +1,12 @@
+// https://www.googleapis.com/youtube/v3/search
+import axios from 'axios';
+import { KEY } from './config/config.json';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
